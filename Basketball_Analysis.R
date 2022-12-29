4445f1d8c7bb9fcd7a03bbadba81a8c5d45cd05c
Saket Kulkarni <95674824+StrangeCoder1729@users.noreply.github.com>
2022-12-29 16:16:17 +0530

    basketball data analysis

---
+++ Basketball_Analysis.R
@@ -0,0 +1,35 @@
+
+
+
+#Salary
+myplot(Salary)
+myplot(Salary/Games)
+myplot(Salary/FieldGoals)
+
+
+
+#In-Game Metrics
+myplot(MinutesPlayed)
+myplot(Points)
+
+
+#In-Game Metrics Normalized
+
+myplot(FieldGoals/Games)
+myplot(FieldGoals/FieldGoalAttempts)
+myplot(FieldGoalAttempts/Games)
+myplot(Points/Games)
+
+
+#Intresting Observation
+myplot(MinutesPlayed/Games)
+myplot(Games)
+
+
+#Time is Valuable
+myplot(FieldGoals/MinutesPlayed)
+myplot(Points/MinutesPlayed)
+
+
+#Player Style
+myplot(Points/FieldGoals)
---
+++ Freethrow_Analysis.R
@@ -0,0 +1,16 @@
+
+
+
+myplot(Freethrows_FTA/Games) #Attemplt of Free throws per game
+
+myplot(Freethrows/Freethrows_FTA) #Accuracy of free throws
+
+myplot((Points-Freethrows)/FieldGoals) #Player Style
+
+
+
+
+
+
+
+
